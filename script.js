89b5d2e8a491e457bf832498ef32c677acfce184
Rovic Castro <193394253+roviccastro@users.noreply.github.com>
2025-01-13 16:56:21 +0800

    Feat: Add random computer choice function
    - To generate computer choice

---
+++ script.js
@@ -0,0 +1,19 @@
+function getComputerChoice(){
+
+  let randomNumber = Math.floor(Math.random() * 3);
+
+  let choice 
+  if (randomNumber === 0){
+    choice = `Rock`;
+  } else if (randomNumber === 1){
+    choice = `Paper`;
+  } else if (randomNumber === 2){
+    choice = `Shoe`;
+  } else {
+    chouce = `Rock`;
+  }
+
+  return choice;
+}
+
+console.log(getComputerChoice());
