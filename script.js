0a0b047e9a98ff5f676d38376a1c9a721ce7d594
Rovic Castro <193394253+roviccastro@users.noreply.github.com>
2025-01-21 23:06:21 +0800

    Refac: Remove ternary game logic
    - Removed it, changing it to ifs

--- script.js
+++ script.js
@@ -16,6 +16,10 @@ let getComputerChoice = () => {
   return choice;
 }
 
+function displayResult(msg){
+  const resultContainer = document.getElementsByClassName("result.container");
+}
+
 function playGame(){
   let humanScore = 0;
   let computerScore = 0;
@@ -38,24 +42,8 @@ function playGame(){
 
   function playRound(humanChoice, computerChoice){  
 
-    humanChoice === computerChoice 
-    ? console.log(`It's a tie!`)
     
-    : humanChoice === `rock` && computerChoice === `scissor`
-    ? console.log(`You win! Rock beats Scissor! Score: ${humanScore += 1}`)
-    : humanChoice === `paper` && computerChoice === `rock`
-    ? console.log(`You win! Paper beats Rock! Score: ${humanScore += 1}`)
-    : humanChoice === `scissor` && computerChoice === `paper`
-    ? console.log(`You win! Scissor beats Paper! Score: ${humanScore += 1}`)
-    : humanChoice === `rock` && computerChoice === `paper`
-    
-    ? console.log(`You lose! Rock beats Scissor! Computer Score: ${computerScore += 1}`)
-    : humanChoice === `paper` && computerChoice === `scissor`
-    ? console.log(`You lose! Paper beats Rock! Computer Score: ${computerScore += 1}`)
-    : humanChoice === `scissor` && computerChoice === `rock`
-    ? console.log(`You lose! Scissor beats Paper! Computer Score: ${computerScore += 1}`)
-    
-    : console.log(`Error`);
+
   }
   
 };
